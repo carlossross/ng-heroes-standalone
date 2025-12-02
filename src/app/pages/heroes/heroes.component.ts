0d0c77264eb4682bc3910f8e68b5e0baaf81d822
carlossross <carlos.rossv@gmail.com>
2025-12-02 16:15:56 -0600

    HttpClient modenr + functional interceptors + heroes from API

--- src/app/pages/heroes/heroes.component.ts
+++ src/app/pages/heroes/heroes.component.ts
@@ -1,15 +1,17 @@
 import { Component, inject } from '@angular/core';
 import { HeroesService } from '../../services/heroes.service';
 import { RouterLink } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-heroes',
-  imports: [RouterLink],
+  imports: [RouterLink, AsyncPipe],
   templateUrl: './heroes.html',
   styleUrl: './heroes.scss',
 })
 export class HeroesComponent {
   private heroesService = inject(HeroesService);
 
-  heroes = this.heroesService.getHeroes();
+  //heroes = this.heroesService.getHeroes();
+  $heroes = this.heroesService.getHeroes();
 }
--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -1,16 +1,23 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroesService {
-  private heores = [
-    { id: 1, name: 'Batman' },
-    { id: 2, name: 'Spiderman' },
-    { id: 3, name: 'Wonder Woman' },
-  ];
+  // private heores = [
+  //   { id: 1, name: 'Batman' },
+  //   { id: 2, name: 'Spiderman' },
+  //   { id: 3, name: 'Wonder Woman' },
+  // ];
+
+  // getHeroes() {
+  //   return this.heores;
+  // }
+
+  private http = inject(HttpClient);
 
   getHeroes() {
-    return this.heores;
+    return this.http.get<any[]>('https://akabab.github.io/superhero-api/api/all.json');
   }
 }
